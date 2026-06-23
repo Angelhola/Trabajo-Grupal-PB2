1d3dbe5baa27ebf69b84d1df35e91ca2efe9c1e8
Valen Romano <valentinaromanop@gmail.com>
2026-06-22 22:32:25 -0300

    Agregue los posibles casos de test para ferreteria

--- Trabajo-Grupal-Ferreteria/test/ar/edu/unlam/pb2/test/test.java
+++ Trabajo-Grupal-Ferreteria/test/ar/edu/unlam/pb2/test/test.java
@@ -7,14 +7,54 @@ import org.junit.Test;
 import ar.edu.unlam.pb2.ferreteria.Pintura;
 
 public class test {
-	
+
 	@Test
 	public void DadoQueExisteUnaFerreteriaYSeAgregaUnProducto() {
 		Ferreteria ferreteria = new Ferreteria();
 		Pintura pintura = new Pintura("pintura", "lokepinte", 200.0, "rojo", 10.0);
 		ferreteria.registrarProducto(pintura);
-		
+
 		assertEquals(ferreteria.getProducto().size(), 1);
 	}
 
+	@Test
+	public void dadoQueExisteUnaFerreteriaCuandoSeAgregaUnProductoConCodigoRepetidoEntoncesSeLanzaUnaExcepcion() {
+
+	}
+
+	@Test
+	public void dadoQueExisteUnaFerreteriaCuandoSeBuscaUnProductoExistenteEntoncesSeObtieneElProducto() {
+
+	}
+
+	public void dadoQueExisteUnaFerreteriaCuandoSeBuscaUnProductoInexistenteEntoncesSeLanzaUnaExcepcion() {
+
+	}
+
+	@Test
+	public void dadoQueExisteUnaFerreteriaCuandoSeEliminaUnProductoExistenteEntoncesElProductoSeEliminaCorrectamente() {
+
+	}
+
+	@Test
+	public void dadoQueExisteUnaFerreteriaCuandoSeIntentaEliminarUnProductoInexistenteEntoncesSeLanzaUnaExcepcion() {
+
+	}
+
+	@Test
+	public void dadoQueExisteUnaFerreteriaCuandoSeRegistranProductosEntoncesNoSePermitenDuplicados() {
+	}
+
+	@Test
+	public void dadoQueExisteUnaFerreteriaCuandoSeObtienenLosProductosEntoncesSeDevuelvenOrdenadosPorNombre() {
+	}
+
+	@Test
+	public void dadoQueExisteUnaFerreteriaCuandoSeRegistraUnProductoConPrecioNegativoEntoncesSeLanzaUnaExcepcion() {
+	}
+
+	@Test
+	public void dadoQueExisteUnaFerreteriaCuandoSeConsultaLaCantidadDeProductosEntoncesSeObtieneLaCantidadCorrecta() {
+
+	}
 }
